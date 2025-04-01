05c0c788c7be75a8ca04591e5a7f93b624eab6f6
Aditya Patel <adityakdpatel@gmail.com>
2025-04-01 18:23:20 -0400

    feat: fixing the big int solution

--- servers/primetime/primetime.go
+++ servers/primetime/primetime.go
@@ -1,8 +1,10 @@
 package primetime
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/big"
 	"net"
 	"sync"
@@ -33,67 +35,91 @@ func PrimeServer(Port string) error {
 	}
 }
 
-func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
-	defer conn.Close()
+type Request struct {
+	Method *string    `json:"method"`
+	Number *BigNumber `json:"number"`
+}
+
+type Response struct {
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
+}
+
+func (req *Request) validFields() bool {
+	if req.Method == nil {
+		return false
+	}
+
+	if *req.Method != "isPrime" {
+		return false
+	}
+
+	if req.Number == nil {
+		return false
+	}
+
+	return true
+}
+
+func handleConnection(c net.Conn, wg *sync.WaitGroup) {
+	defer c.Close()
 	defer wg.Done()
 
-	decoder := json.NewDecoder(conn)
-	encoder := json.NewEncoder(conn)
+	reader := bufio.NewReader(c)
 
 	for {
-		type Request struct {
-			Method string    `json:"method"`
-			Number BigNumber `json:"number"`
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Print(err.Error())
+			c.Write([]byte(err.Error()))
+			break
 		}
+
+		log.Printf("Recieved: %v", message)
 
 		var request Request
-		if err := decoder.Decode(&request); err != nil {
-			sendMalformedResponse(encoder)
-			return
+		err = json.Unmarshal([]byte(message), &request)
+
+		// handle validation
+		if err != nil {
+			log.Print(err.Error())
+			c.Write([]byte(err.Error()))
+			break
 		}
 
-		if request.Method != "isPrime" {
-			fmt.Printf("[Primetime] Received invalid method: %s\n", request.Method)
-			sendMalformedResponse(encoder)
-			return
+		if !request.validFields() {
+			log.Print("Invalid Fields")
+			c.Write([]byte("Invalid Fields"))
+			break
 		}
 
+		// convert the string to bigInt
 		if request.Number.IsFloat {
-			fmt.Printf("[Primetime] Received float number, expected integer: %+v\n", request.Number)
-			sendMalformedResponse(encoder)
-			return
+			res := Response{Method: "isPrime", Prime: false}
+			handleResponse(c, res)
+			break
 		}
 
-		if request.Number.BigInt.Sign() < 0 {
-			fmt.Printf("[Primetime] Received negative number: %+v\n", request.Number)
-			sendMalformedResponse(encoder)
-			return
-		}
-
-		fmt.Printf("[Primetime] Received request: method=%s, number=%s\n",
-			request.Method, request.Number.BigInt.String())
-
-		response := map[string]interface{}{
-			"method": "isPrime",
-			"prime":  isPrime(request.Number.BigInt),
-		}
-
-		if err := encoder.Encode(response); err != nil {
-			fmt.Printf("[Primetime] Error sending response: %v\n", err)
-			return
-		}
+		// handle response
+		res := Response{Method: "isPrime", Prime: isPrime(*request.Number.BigInt)}
+		handleResponse(c, res)
 	}
 }
 
-func isPrime(n *big.Int) bool {
-	return n.ProbablyPrime(20)
+func handleResponse(c net.Conn, res Response) {
+	resJson, err := json.Marshal(res)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Response: ", string(resJson)+"\n")
+	c.Write([]byte(string(resJson) + "\n"))
 }
 
-func sendMalformedResponse(encoder *json.Encoder) {
-	malformedResponse := map[string]interface{}{
-		"error": "malformed request",
-	}
-	_ = encoder.Encode(malformedResponse)
+func isPrime(n big.Int) bool {
+	return n.ProbablyPrime(10)
 }
 
 type BigNumber struct {
@@ -102,6 +128,7 @@ type BigNumber struct {
 }
 
 func (n *BigNumber) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal into a string first
 	numStr := string(data)
 
 	floatValue := new(big.Float)
